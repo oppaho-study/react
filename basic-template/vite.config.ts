832ab7ed6ecc3288c9cab20eac092a6bf8f1d553
oppaho03 <oppaho03@gmail.com>
2025-07-02 20:29:43 +0900

    add /basic-template

---
+++ basic-template/vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/",
+  resolve: {
+    extensions: [ '.ts', '.tsx' ],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@public' : path.resolve(__dirname, './public'),
+    },
+  },
+  build: {
+    polyfillModulePreload: true,
+    outDir: 'dist',
+    assetsDir: 'assets',
+    assetsInlineLimit: 4096,
+    rollupOptions: {
+      // input: {},
+      output: {
+        entryFileNames: 'js/[name]min.js',
+        assetFileNames: 'assets/[name].[ext]',
+      }
+    },
+  },
+  server: {
+    host: 'localhost',
+    port: 9000,
+    open: false,
+    cors: true,
+  }
+});
